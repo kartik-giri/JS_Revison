eb60d27683d636e5679a9a343768a4c4e032e844
kartik-giri <kartikgiri1t30@gmail.com>
2025-09-26 23:30:43 +0530

    created CLI to ount file in a given file

---
+++ NodeLearning/cliAssignment/index.js
@@ -0,0 +1,42 @@
+//CLI command strcuture
+//program [command] [arguments] [options]
+
+/*
+program → the main executable (e.g., git, node app.js)
+command → subcommand (e.g., init, commit, push)
+arguments → required or optional values (e.g., a file name)
+options (flags) → extra settings (usually start with - or --)
+*/
+
+/*
+✅ So the structure of a CLI command is always:
+Program → Command → Arguments → Options
+*/
+
+//<name> => There will be a required argument named name.
+
+const { program } = require('commander');
+const fs = require('fs');
+
+//Commander uses a design style called a fluent API (also called method chaining).
+//This means that every method (like .option(), .argument(), etc.) returns the same program object so you can "chain" calls together.
+
+program.name("CLI assignment")
+       .description("ClI for counting number of words in a given file")
+       .version("1.0.0")
+       .argument("<filePath>","path of the given file")
+       .action((filePath)=>{
+        fs.readFile(filePath, "utf-8", (err,content)=>{
+            if(err){
+                console.log("Can read file cause of this error:",err);
+            }else{
+                let wordsArr = content.trim().split(/\s+/);
+                console.log("The words in given file are:",wordsArr.length);
+            }
+        })
+       })
+
+
+program.parse();//It’s the method that tells Commander:
+//“Okay, take everything the user typed in the terminal and parse it according to the arguments and options I defined.”
+//Without program.parse(), Commander won’t read or process anything.
