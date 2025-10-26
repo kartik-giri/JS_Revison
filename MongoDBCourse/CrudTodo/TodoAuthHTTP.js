a8e0dbc6917852b89e1cd0190f3d9791565da1bd
kartik-giri <kartikgiri1t30@gmail.com>
2025-10-27 00:06:18 +0530

    Add improvemnts to the todo backend

--- MongoDBCourse/CrudTodo/TodoAuthHTTP.js
+++ MongoDBCourse/CrudTodo/TodoAuthHTTP.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userModel, todosModel } = require("./DB");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const {z} = require('zod');
 
 /*
 Under the hood:
@@ -21,19 +23,43 @@ app.use(express.json());
 const jwtSecret = "IamSecret";
 
 app.post("/sign-up", async (req, res) => {
+  const requireBody = z.object({
+    userEmail: z.string().min(3).max(30).email(),
+    userName: z.string().min(3).max(30),
+    userPassword: z.string().min(3).max(30)
+  })
+
+  //Check if the user input passes the schema checks.
+  const {success} = requireBody.safeParse(req.body);
+
+  if(!success){
+    res.status(400).json({
+      message: "Incorect format"
+    })
+    return;
+  }
+
   const userEmail = req.body.userEmail;
   const userName = req.body.userName;
   const userPassword = req.body.userPassword;
 
-  await userModel.create({
-    email: userEmail,
-    password: userPassword,
-    username: userName,
-  });
+  try {
+    const hashPassword = await bcrypt.hash(userPassword,10);
 
-  res.status(200).json({
-    message: `${userName} You are sign up!`,
-  });
+    await userModel.create({
+      email: userEmail,
+      password: hashPassword,
+      username: userName,
+    });
+
+    res.status(200).json({
+      message: `${userName} You are sign up!`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Error occured while creating new sign-up entry.",
+    });
+  }
 });
 
 app.post("/log-in", async (req, res) => {
@@ -42,12 +68,24 @@ app.post("/log-in", async (req, res) => {
 
   const findUser = await userModel.findOne({
     email: userEmail,
-    password: userPassword,
   });
 
   console.log(findUser);
 
-  if (findUser) {
+  if(!findUser){
+    res.status(400).json({
+      message: "User is not signed up!",
+    });
+    return;
+  }
+  
+  //Crucial to check if given password matches with the DB hashed password.
+  //salt+password is hashed to get unique hash.
+  //salt is added in front of gnerated hash.
+  //So that we can get the salt. And hash the password and salt together to get the hash and see if it matches the stored hash.
+  const matchPassword = await bcrypt.compare(userPassword, findUser.password);
+
+  if (matchPassword) {
     let token = null;
     token = jwt.sign({ id: findUser._id }, jwtSecret); //._id is unique idetifer given to every document by mongoDB
     res.json({
@@ -64,92 +102,67 @@ app.post("/log-in", async (req, res) => {
 //The server doesn’t care about restarts, because no session state is stored in memory.
 const auth = (req, res, next) => {
   const token = req.headers.authorization;
-  if(!token){
+  if (!token) {
     res.status(400).json({
-        message: "Token is not specified!"
-    })
+      message: "Token is not specified!",
+    });
   }
-  try{
-    let jwtData = jwt.verify(token,jwtSecret);
+  try {
+    let jwtData = jwt.verify(token, jwtSecret);
     let userId = jwtData.id;
     req.userId = userId;
     next();
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
-        message:`Error occured in auth middleware!`
-    })
+      message: `Error occured in auth middleware!`,
+    });
   }
 };
 
-app.post("/todo", auth ,async (req, res) => {
-    let userId = req.userId;
-    
-    const todoTitle= req.body.todoTitle;
-    const todoDescription = req.body.todoDescription;
-    
-    try{
-    const userFound = userModel.findOne({
-        _id: userId
-    })
+app.post("/todo", auth, async (req, res) => {
+  let userId = req.userId;
 
-    if(userFound){
-        await todosModel.create({
-            title: todoTitle,
-            description: todoDescription,
-            id: userId
-        })
+  const todoTitle = req.body.todoTitle;
+  const todoDescription = req.body.todoDescription;
 
-        const todoFound = await todosModel.findOne({
-            title: todoTitle,
-            description: todoDescription,
-            id: userId
-        })
-
-        console.log(todoFound);
-    }
+  try {
+    await todosModel.create({
+      title: todoTitle,
+      description: todoDescription,
+      id: userId,
+    });
 
     res.status(200).json({
-        message: `${todoTitle} Todo is added`
-    })}catch(err){
-        res.status(200).json({
-            message:`Error occured in todo post request`
-        })
-    }
-
+      message: `${todoTitle} Todo is added`,
+    });
+  } catch (err) {
+    res.status(200).json({
+      message: `Error occured in todo post request`,
+    });
+  }
 });
 
 app.get("/todos", auth, async (req, res) => {
-    let userid = req.userId;
+  let userid = req.userId;
 
-    try{
-        const userTodos = await todosModel.find({
-            id:userid
-        })
+  try {
+    const userTodos = await todosModel.find({
+      id: userid,
+    });
 
-        if(userTodos){
-            res.status(200).json({
-                message: userTodos
-            })
-        }
-    }catch(err){
-       res.status(400).json({
-        message: `Error occured in todos get request.`
-       }) 
+    if (userTodos) {
+      res.status(200).json({
+        message: userTodos,
+      });
     }
+  } catch (err) {
+    res.status(400).json({
+      message: `Error occured in todos get request.`,
+    });
+  }
 });
 
 app.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 {
