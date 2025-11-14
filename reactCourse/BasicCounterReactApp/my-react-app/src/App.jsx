a76d4b8b6ba00082f1ee397d5de7453b4a6985c8
kartik-giri <kartikgiri1t30@gmail.com>
2025-11-14 23:13:16 +0530

    Learn useState and useEffect

--- reactCourse/BasicCounterReactApp/my-react-app/src/App.jsx
+++ reactCourse/BasicCounterReactApp/my-react-app/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 
 function App() {
+  //We can change the internal elements of an array even though the vairbale is const.
+  //Set and setCount are vairbales of array which we are getting by destructring an array.
 const [count, setCount] = useState(0);
 
 const onClickHadler = ()=>{
   setCount(count+1); //changed the state, react will do re-render auomatically. count++ doesn't work here cause it is post incremental
 }
+//Returning a markup
 return (
     <>
     <div>
@@ -17,4 +20,6 @@ return (
   )
 }
 
+//It is syntax of module js we are exporting the function directly and can import directly using import syntax.
+//The export default keywords specify the main component in the file. which is exporting
 export default App
---
+++ reactCourse/LearnReactApp/ReactLearningApp/src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+//useState is a Hook that lets you add state to functional components. It returns an array with the current state
+//and a function to update it.
+const App = () => {
+  const [flag, setFlag] = useState(true);
+  
+
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      setFlag((flag)=>{
+        return !flag
+      })
+    },5000)
+    return ()=> clearInterval(id)
+  },[])
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      {flag?<EffectLearn/>:null}
+      <Counter/>
+    </>
+  );
+};
+
+//When ever state changes react calls the component with new state.
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  //will redner after mounted, unmounted and re-render when count state changes
+  useEffect(()=>{
+    alert("useEffect runs when count state changes");
+
+    return ()=>{
+      alert("clear the preivous effect")
+    }
+  },[count])
+  const increaseHandler = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseHandler = () => {
+    setCount(count - 1);
+  };
+
+  const resetHandler = () => {
+    setCount(0);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>Decrease</button>
+      <button onClick={resetHandler}>Reset</button>
+    </>
+  );
+};
+
+const EffectLearn=()=>{
+  const [count, setCount] = useState(0);
+  
+  //React runs useEffect twice in development:
+  //
+  useEffect(()=>{
+    const id = setInterval(()=>{
+    setCount((count)=>{return count+1});
+  },1000)
+  //stops the interval when the component unmounts or before the effect runs again.
+  //Question We are starting the clock when component is mounted or rendered for the first time BUT?
+  //Are we stopping the clock when component is unmounted or deleted from the DOM?
+  //When unmounts clear the interval
+  return ()=>{
+    clearInterval(id);}
+  },[])
+
+  return (
+    <>
+    <h1>Clock: {count}</h1>
+    </>
+  )
+}
+export default App;
