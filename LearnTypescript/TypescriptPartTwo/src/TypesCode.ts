92da6afe7017021ac09f3c746567dd383763ead1
kartik-giri <kartikgiri1t30@gmail.com>
2025-12-17 13:49:48 +0530

    learnt types unions and intersection clearly

--- LearnTypescript/TypescriptPartTwo/src/TypesCode.ts
+++ LearnTypescript/TypescriptPartTwo/src/TypesCode.ts
@@ -1,3 +1,8 @@
+//1. In Vanderkam's Effective TypeScript, he advises us to think of types as set of values
+//2. For example, the type number is simply the set of all numbers (an infinite set)
+//3. The same apply for "composite"1 types created with the keywords interface and type
+//4. TypeScript's types are open (and not sealed like in some other programming languages)
+
 type User = {
 	firstName: string;
 	lastName: string;
@@ -5,20 +10,40 @@ type User = {
 }
 
 type Hacker = {
-    firstName:string;
-    lastName: string;
-    age:number;
     organisation:string
 }
 
-//Union-> uses to assign two or more type checks to variable.
+//Union-> in maths The union of sets combines all unique elements from two or more sets into a single new set
+//Which means in all subset of types will be unique.
+
+/* --> this will be unique
+    firstName:"kartik",
+    lastName:"giri",
+    age:23,
+*/
+
+/*-->this will be unique too 
+ organisation:"bdjcbdj"
+*/
+
+/* --> this will be unique also
+    firstName:"kartik",
+    lastName:"giri",
+    age:23,
+    organisation:"bdjcbdj"
+*/
+
 const userKartik:User|Hacker={
     firstName:"kartik",
     lastName:"giri",
     age:23,
+    organisation:"bdjcbdj"
 }
 
-//Intersection-> is used to create the new type which have all properties of tow or more types.
+//Intersection-> in maths sets this means creting the set of common value between 2 sets.
+//1. it's worth reminding ourselves that a type isn't the collection of properties but the set of all values with those properties.
+//2. so the User type contain firstName, lastName, age and organization set AND Hacker set contain  firstName:string; lastName: string; age:number; organisation:string
+
 type EthicalHacker = User & Hacker;
 
 const employee:EthicalHacker = {
