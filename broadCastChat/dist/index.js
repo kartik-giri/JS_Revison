0ab8d203eddf0fc3fe83459ccab7ce7d0193c15b
kartik-giri <kartikgiri1t30@gmail.com>
2026-02-11 18:33:26 +0530

    continue building chat app

--- broadCastChat/dist/index.js
+++ broadCastChat/dist/index.js
@@ -1,23 +1,44 @@
+//Useres will jon in romm and server have to broadcast msg only to certain room.
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-let count = 0;
-let allSockets = [];
+//room -> set of sockets {socket1, socket2 ...}
+let allSocket = new Map();
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    count = count + 1;
-    console.log(`Number of clients connected to webSocket server ${count}`);
-    //Catch the messsage send by client.
-    socket.on("message", (ev) => {
-        console.log(`Message is received ${ev.toString()}`);
-        //Broadcast msg to all the socket
-        allSockets.forEach((elem) => {
-            elem.send(`${ev.toString()} send from server`);
+    socket.on("message", (message) => {
+        //Client will send certain msg passed by schema.
+        //the message parameter is not guaranteed to be a string. that's why we have to first convert it in to string and than object.
+        const messageString = message.toString();
+        const messageObj = JSON.parse(messageString);
+        if (messageObj.type == "join") {
+            if (!allSocket.has(messageObj.payload.roomId)) {
+                allSocket.set(messageObj.payload.roomId, new Set()); //setting key
+            }
+            allSocket.get(messageObj.payload.roomId)?.add(socket); //setting value 
+            socket.roomId = messageObj.payload.roomId;
+            console.log(`User join the room ${messageObj.payload.roomId}`);
+        }
+        //Client wants to send to chat. first get room  of user in which it wants to sends.
+        if (messageObj.type == "chat") {
+            let roomId = socket.roomId;
+            if (roomId) {
+                allSocket.get(roomId)?.forEach((elemSocket) => {
+                    elemSocket.send(messageObj.payload.message);
+                });
+            }
+        }
+        //Handles disconnection
+        socket.on("close", () => {
+            let roomId = socket.roomId;
+            if (roomId) {
+                allSocket.get(roomId)?.delete(socket);
+            }
+            else {
+                return;
+            }
+            if (allSocket.get(roomId)?.size === 0) {
+                allSocket.delete(roomId);
+            }
+            console.log(`Total number of sockets: ${allSocket.get(roomId)?.size}`);
         });
-    });
-    socket.on("close", () => {
-        allSockets = allSockets.filter((elem) => {
-            return elem !== socket;
-        });
-        console.log(`Numner of sockets ${allSockets.length}`);
     });
 });
--- broadCastChat/src/index.ts
+++ broadCastChat/src/index.ts
@@ -1,32 +1,59 @@
-import {WebSocketServer, WebSocket} from "ws";
+//Useres will jon in romm and server have to broadcast msg only to certain room.
+
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
-let count = 0;
-let allSockets: WebSocket[] = [] 
+interface customSocketType extends WebSocket{
+    roomId ?: string
+}
 
-wss.on("connection", (socket)=>{
+//room -> set of sockets {socket1, socket2 ...}
+let allSocket = new Map<string, Set<WebSocket>> ();
 
-    allSockets.push(socket);
+wss.on("connection", (socket: customSocketType)=>{
 
-    count = count +1;
-    console.log(`Number of clients connected to webSocket server ${count}`)
+    socket.on("message", (message)=>{
+        //Client will send certain msg passed by schema.
+        //the message parameter is not guaranteed to be a string. that's why we have to first convert it in to string and than object.
+        
+        const messageString = message.toString();
+        const messageObj = JSON.parse(messageString);
 
-    //Catch the messsage send by client.
-    socket.on("message", (ev)=>{
-        console.log(`Message is received ${ev.toString()}`);
+        if(messageObj.type == "join"){
 
-        //Broadcast msg to all the socket
-        allSockets.forEach((elem)=>{
-            elem.send(`${ev.toString()} send from server`)
-        })
+            if(!allSocket.has(messageObj.payload.roomId)){
+            allSocket.set(messageObj.payload.roomId, new Set()); //setting key
+            }
+            allSocket.get(messageObj.payload.roomId)?.add(socket); //setting value 
+            socket.roomId = messageObj.payload.roomId
+
+            console.log(`User join the room ${messageObj.payload.roomId}`)
+        }
+
+        //Client wants to send to chat. first get room  of user in which it wants to sends.
+        if(messageObj.type == "chat"){
+            let roomId = socket.roomId;
+            if(roomId){
+            allSocket.get(roomId)?.forEach((elemSocket)=>{
+                elemSocket.send(messageObj.payload.message)
+            })}
+        }
     })
 
     socket.on("close", ()=>{
-        allSockets = allSockets.filter((elem)=>{
-            return elem !== socket
-        })
-        console.log(`Numner of live sockets ${allSockets.length}`)
-    })
-})
+            let roomId = socket.roomId;
 
+            if(roomId){
+            allSocket.get(roomId)?.delete(socket);
+            }else{
+                return
+            }
+
+            if(allSocket.get(roomId)?.size ===0){
+                allSocket.delete(roomId)
+            }
+
+            console.log(`Total number of sockets: ${allSocket.get(roomId)?.size}`)
+        })
+})
